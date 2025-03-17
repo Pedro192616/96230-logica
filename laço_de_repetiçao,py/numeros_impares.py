b9977aad8c037ffb43714449f33b44510bee5ec5
Pedro192616 <pedroribalc.05@gmail.com>
2025-03-17 15:53:53 -0300

    Add files via upload

--- laço_de_repetiçao,py/numeros_impares.py
+++ laço_de_repetiçao,py/numeros_impares.py
@@ -3,7 +3,7 @@ os.system("cls | | clear")
 
 print("APENAS NUMEROS IMPARES:")
 
-for i in range (100,121):
-    if i % 1 == 1:
+for i in range (1,21):
+    if i % 2 == 1:
       print (i)
 print ("FIM DO PROGRAMA")
